df64a09f57e0769d66598e5a5701acf9f658a897
Raghu Betina <raghu@firstdraft.com>
2018-05-31 14:25:43 +0000

    Direct association between movies and favourites

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,6 +1,8 @@
 class Favourite < ApplicationRecord
   # Direct associations
 
+  belongs_to :movie
+
   # Indirect associations
 
   # Validations
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ApplicationRecord
   # Direct associations
 
+  has_many   :favourites,
+             :dependent => :destroy
+
   has_many   :castings,
              :dependent => :destroy
 
